76ad0f44c35d8531444739b9319753554b4bb865
thramania <thramania@hanmail.net>
2024-01-26 11:48:22 +0900

    controller

--- src/main/java/com/dev/IBIOECommerce/controller/AdminController.java
+++ src/main/java/com/dev/IBIOECommerce/controller/AdminController.java
@@ -8,9 +8,160 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
-	@GetMapping({"" , "/"})
+	@GetMapping({"" , "/", "/index"})
 	public String adminIndex() {
 		
 		return "administration/common/index";
 	} 
+	
+	@GetMapping("/memberRegistrationCheck")
+	public String memberRegistrationCheck() {
+		
+		return "administration/ibio/memberRegistrationCheck";
+	}
+	
+	@GetMapping("/dealerManager")
+	public String dealerManager() {
+		
+		return "administration/ibio/dealerManager";
+	}
+	
+	@GetMapping("/memberManager")
+	public String memberManager() {
+		
+		return "administration/ibio/memberManager";
+	}
+	
+	@GetMapping("/memberInquiryManager")
+	public String memberInquiryManager() {
+		
+		return "administration/ibio/memberInquiryManager";
+	}
+	
+	@GetMapping("/dealerInquiryManager")
+	public String dealerInquiryManager() {
+		
+		return "administration/ibio/dealerInquiryManager";
+	}
+	
+	@GetMapping("/refundManager")
+	public String refundManager() {
+		
+		return "administration/ibio/refundManager";
+	}
+	
+	@GetMapping("/couponManager")
+	public String couponManager() {
+		
+		return "administration/ibio/couponManager";
+	}
+	
+	@GetMapping("/siteManager")
+	public String siteManager() {
+		
+		return "administration/ibio/siteManager";
+	}
+	
+	@GetMapping("/eventManager")
+	public String eventManager() {
+		
+		return "administration/ibio/eventManager";
+	}
+	
+	@GetMapping("/noticeManager")
+	public String noticeManager() {
+		
+		return "administration/ibio/noticeManager";
+	}
+	
+	@GetMapping("/faqManager")
+	public String faqManager() {
+		
+		return "administration/ibio/faqManager";
+	}
+	
+	@GetMapping("/bannerManager")
+	public String bannerManager() {
+		
+		return "administration/ibio/bannerManager";
+	}
+	
+	@GetMapping("/accessManager")
+	public String accessManager() {
+		
+		return "administration/ibio/accessManager";
+	}
+	
+	@GetMapping("/logManager")
+	public String logManager() {
+		
+		return "administration/ibio/logManager";
+	}
+	
+	@GetMapping("/fileManager")
+	public String fileManager() {
+		
+		return "administration/ibio/fileManager";
+	}
+	
+	@GetMapping("/mamberCalculationManager")
+	public String mamberCalculationManager() {
+		
+		return "administration/ibio/mamberCalculationManager";
+	}
+	
+	@GetMapping("/memberSellingManager")
+	public String memberSellingManager() {
+		
+		return "administration/ibio/memberSellingManager";
+	}
+	
+	@GetMapping("/dealerSellingManager")
+	public String dealerSellingManager() {
+		
+		return "administration/ibio/dealerSellingManager";
+	}
+	
+	@GetMapping("/dealerCalculationManager")
+	public String dealerCalculationManager() {
+		
+		return "administration/ibio/dealerCalculationManager";
+	}
+	
+	@GetMapping("/billManager")
+	public String billManager() {
+		
+		return "administration/ibio/billManager";
+	}
+	
+	@GetMapping("/ibioProductManager")
+	public String ibioProductManager() {
+		
+		return "administration/ibio/ibioProductManager";
+	}
+	
+	@GetMapping("/ibioProductOverview")
+	public String ibioProductOverview() {
+		
+		return "administration/ibio/ibioProductOverview";
+	}
+	
+	@GetMapping("/dealerProductManager")
+	public String dealerProductManager() {
+		
+		return "administration/ibio/dealerProductManager";
+	}
+	
+	@GetMapping("/ibioProductInfoDumpManager")
+	public String ibioProductInfoDumpManager() {
+		
+		return "administration/ibio/ibioProductInfoDumpManager";
+	}
+	
+	@GetMapping("/ibioProductFileDumpManager")
+	public String ibioProductFileDumpManager() {
+		
+		return "administration/ibio/ibioProductFileDumpManager";
+	}
+	
 }
--- src/main/java/com/dev/IBIOECommerce/controller/DealerController.java
+++ src/main/java/com/dev/IBIOECommerce/controller/DealerController.java
@@ -2,17 +2,126 @@ package com.dev.IBIOECommerce.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/dealer")
 public class DealerController {
 
-	@GetMapping({"", "/index"})
-	@ResponseBody
+	@GetMapping({"", "/index", "/"})
 	public String dealerIndex() {
 		
-		return "dealerIndex";
+		return "administration/common/index";
+	}
+	
+	@GetMapping("/memberInquiryManager")
+	public String memberInquiryManager() {
+		
+		return "administration/dealer/memberInquiryManager";
+	}
+	
+	@GetMapping("/refundManager")
+	public String refundManager() {
+		
+		return "administration/dealer/refundManager";
+	}
+	
+	@GetMapping("/couponManager")
+	public String couponManager() {
+		
+		return "administration/dealer/couponManager";
+	}
+	
+	@GetMapping("/dealerProductManager")
+	public String dealerProductManager() {
+		
+		return "administration/dealer/dealerProductManager";
+	}
+	
+	@GetMapping("/dealerProductDetail")
+	public String dealerProductDetail() {
+		
+		return "administration/dealer/dealerProductDetail";
+	}
+	
+	@GetMapping("/dealerProductInsert")
+	public String dealerProductInsert() {
+		
+		return "administration/dealer/dealerProductInsert";
+	}
+	
+	@PostMapping("/dealerProductUpdate")
+	public String dealerProductUpdate() {
+		
+		return "administration/dealer/dealerProductManager";
+	}
+	
+	@GetMapping("/dealerProductDelete")
+	public String dealerProductDelete() {
+		
+		return "administration/dealer/dealerProductManager";
+	}
+	
+	@GetMapping("/dealerProductInfoDumpManager")
+	public String dealerProductInfoDumpManager() {
+		
+		return "administration/dealer/dealerProductInfoDumpManager";
+	}
+
+	@GetMapping("/dealerProductFileDumpManager")
+	public String dealerProductFileDumpManager() {
+		
+		return "administration/dealer/dealerProductFileDumpManager";
+	}
+	
+	@GetMapping("/dealerPaymentManager")
+	public String dealerPaymentManager() {
+		
+		return "administration/dealer/dealerPaymentManager";
+	}
+	
+	@GetMapping("/dealerSellingManager")
+	public String dealerSellingManager() {
+		
+		return "administration/dealer/dealerSellingManager";
+	}
+	
+	@GetMapping("/dealerCalculationManager")
+	public String dealerCalculationManager() {
+		
+		return "administration/dealer/dealerCalculationManager";
+	}
+	
+	@GetMapping("/dealerBillManager")
+	public String dealerBillManager() {
+		
+		return "administration/dealer/dealerBillManager";
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
